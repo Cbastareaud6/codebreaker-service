7da4f4d68a8a78c64fdadb4953740a451c4e3a76
Cathy Bastareaud <catybastareaud@gmail.com>
2022-03-21 09:59:11 -0600

    Game level performance query complete

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/view/GamePerformance.java
@@ -0,0 +1,63 @@
+package edu.cnm.deepdive.codebreaker.model.view;
+
+import edu.cnm.deepdive.codebreaker.model.entity.User;
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
+
+@SuppressWarnings("SqlResolve")
+@Entity
+@Immutable
+@Subselect("SELECT  * FROM  game_performance")
+public class GamePerformance {
+
+  @Id
+  @Column(name = "game_id")
+  private UUID id;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date created;
+
+  private int poolSize;
+
+  private int length;
+
+  private int guessCount;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  public int getPoolSize() {
+    return poolSize;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public int getGuessCount() {
+    return guessCount;
+  }
+}
--- src/main/java/edu/cnm/deepdive/codebreaker/service/PerformanceService.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/service/PerformanceService.java
@@ -1,27 +1,36 @@
 package edu.cnm.deepdive.codebreaker.service;
 
-import edu.cnm.deepdive.codebreaker.model.dao.PerformanceRepository;
-import edu.cnm.deepdive.codebreaker.view.Performance;
+import edu.cnm.deepdive.codebreaker.model.dao.GamePerformanceRepository;
+import edu.cnm.deepdive.codebreaker.model.view.GamePerformance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PerformanceService implements AbstractPerformanceService {
 
-  private final PerformanceRepository repository;
+  private final GamePerformanceRepository repository;
 
   @Autowired
-  public PerformanceService(PerformanceRepository repository) {
+  public PerformanceService(GamePerformanceRepository repository) {
     this.repository = repository;
   }
 
   @Override
-  public Iterable<Performance> getRankingsByDuration(int poolSize, int codeLength, int count) {
-    return repository.getRankingsByTime(poolSize, codeLength, count);
+  public Iterable<GamePerformance> getGameRankingsByDuration(int poolSize, int codeLength, int count) {
+    Pageable pageable = PageRequest.of(0, count,
+        Sort.by("duration").ascending().and(Sort.by("guess_count").ascending())) ;
+
+    return repository.findAllByPoolSizeAndLength(poolSize, codeLength, pageable);
   }
 
   @Override
-  public Iterable<Performance> getRankingsByGuessCount(int poolSize, int codeLength, int count) {
-    return repository.getRankingsByGuesses(poolSize, codeLength, count);
+  public Iterable<GamePerformance> getGameRankingsByGuessCount(int poolSize, int codeLength, int count) {
+    Pageable pageable = PageRequest.of(0, count,
+        Sort.by("guess_count").ascending().and(Sort.by("duration").ascending())) ;
+
+    return repository.findAllByPoolSizeAndLength(poolSize, codeLength, pageable);
   }
 }
